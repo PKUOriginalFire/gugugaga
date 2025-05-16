b8c8276821ba6c35c7660a3d4989e42fd5b21e15
Jiayi Zhuang <wybxc@qq.com>
2025-05-17 00:03:48 +0800

    fix: 修正 xpra 启动命令的参数格式

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ chmod 700 $XDG_RUNTIME_DIR
 
 export DISPLAY=:0
 
-xpra start --bind-tcp=0.0.0.0:14500 --no-daemon \
+xpra start :0 --bind-tcp=0.0.0.0:14500 --no-daemon \
     --start-child="bash -c \"( $HOME/QQ/AppRun & $HOME/gugugaga --ws-server=ws://danmaku-server:5099/danmaku & wait -n; kill 0 ) 2>/dev/null\"" \
     --exit-with-child=yes
